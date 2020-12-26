5b205d2321cdd80b68b86197dc19007e2311dd37
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2020-12-27 02:13:46 +0700

    [Update]

--- demo/api.py
+++ demo/api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request, jsonify
+from flask import request, render_template, jsonify
 import db_connection
 import processing
 
@@ -51,6 +51,22 @@ def get_skillgraph():
     rr = processing.processing_skill_value(processing.processing_subject(processing.get_score("ST0003")))
     return jsonify(rr)
 
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        id = request.form['id'] 
+        password = request.form['password']
+        
+        db = db_connection.db_connection()
+        data = db.query("SELECT * FROM `user` WHERE user_id = \""+ id + "\" and user_password = \"" + password + "\"")
+        if len(data) == 0:
+            pass
     
+@app.route('/index')
+def index():
+    return render_template("index_test.html")
+
 if __name__ == "main":
     app.run()
